556b162d958208e2ac5556f2d00ff4215ab37c83
giu1s <djgiulia04@gmail.com>
2022-05-31 19:50:15 +0200

    created home an user component

--- sports-plan/src/app/app-routing.module.ts
+++ sports-plan/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo:'/home', pathMatch: 'full'},
+  // { 
+  {path : '', component: HomeComponent},
+  {path : 'user-details', component: UserDetailsComponent}
+  //   children: [
+  //     { path: '', redirectTo: 'home', pathMatch: 'full'},
+  //     { path: 'home', component: HomeComponent}
+  //   ]
+  // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- sports-plan/src/app/app.module.ts
+++ sports-plan/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
